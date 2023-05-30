3a53a705bc8b612fa40335bd5faf057415be6cb2
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-30 15:14:33 +0200

    added backgorund picture

--- src/OrderGrid.js
+++ src/OrderGrid.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 export default function OrderGrid(props) {
-  return ( 
-      <div>Ordering...</div>
-   );
+  return (
+    <div className="order-grid display-none-when-extra-small">
+      <a
+        id="order-links"
+        href={props.link1}
+        target="_blank"
+        rel="noreferrer"
+        title={props.linkTitle1}
+      >
+        <div className="high-light-text">
+          <span>
+            <img
+              className="icon-adjustments"
+              src={props.logo1}
+              alt="logo-one"
+            ></img>
+          </span>
+          <span style={{ color: props.textColor }}>{props.content1}</span>
+        </div>
+      </a>
+      <a
+        id="order-links"
+        className="float-start high-light-text"
+        href={props.link2}
+        target="_blank"
+        rel="noreferrer"
+        title={props.linkTitle2}
+      >
+        <div className="">
+          <span>
+            <img
+              className="icon-adjustments"
+              src={props.logo2}
+              alt="logo-two"
+            ></img>
+          </span>
+          <span style={{ color: props.textColor }}>{props.content2}</span>
+        </div>
+      </a>
+    </div>
+  );
 }
