999114367fa8ef1bcaaef0f0cc9d0136fbb1a238
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-31 23:44:07 +0200

    created menus

---
+++ src/CreamCrownMenu.js
@@ -0,0 +1,45 @@
+import React from "react";
+import MenuItemGrid from "./MenuItemGrid";
+import PreviousButton from "./PreviousButton";
+import NextButton from "./NextButton";
+export default function CreamCrownMenu(props) {
+  const menuItems = [
+    { food: "Pearl Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "Grass Jellly Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "French Pudding Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "QQ Style Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "Coconut Jelly Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "Coffee Jelly Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "Oreo Potted Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "Red Bean Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "Panda Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "Crystal Milk Tea", prices: [{ price: 4.2 }] },
+    { food: "Caramel Pearl Milk Tea", prices: [{ price: 4.7 }] },
+    { food: "Red Bean Almond Tea", prices: [{ price: 4.7 }] },
+    { food: "Fresh Taro Milk Tea", prices: [{ price: 5.2 }] },
+    { food: "Taro Bobo Milk Tea", prices: [{ price: 5.4 }] },
+  ];
+
+  return (
+    <div className="CreamCrownMenu" id={props.menuDetails.menuHref}>
+      <div className="stay-middle-section-padding">
+        <div className="previous-menu-next-grid">
+          <PreviousButton link={props.linkToPreviousPage} />
+          <div className="menu-card">
+            <h3 className="itallic-subHeading">
+              {props.menuDetails.menuTitle}
+            </h3>
+            {menuItems.map((item) => (
+              <MenuItemGrid
+                key={item.food}
+                food={item.food}
+                price={item.prices}
+              />
+            ))}
+          </div>
+          <NextButton link={props.linkToNextPage} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/PageHero.js
+++ src/PageHero.js
@@ -1,27 +1,27 @@
 import React from "react";
-import FacebookIcon from "./images/facebook.png";
-import InstagramIcon from "./images/instagram.png";
 import MenuButton from "./MenuButton";
 import OrderGrid from "./OrderGrid";
 import DeliverooLogo from "./images/deliveroo.png";
 import PhoneIcon from "./images/phone.png";
+import FacebookIcon from "./images/facebook.png";
+import InstagramIcon from "./images/instagram.png";
 
 export default function PageHero(props) {
   return (
     <div className="PageHero">
-      <div className="stay-middle-section-padding">
+      <div className="container">
         <div className="pagehero-top-grid">
           <a
             className="display-none-when-extra-small"
-            href="https://www.facebook.com/adastralfishnchips"
+            href="https://www.facebook.com/t4unitedkingdom/"
             target="_blank"
             rel="noreferrer"
-            title="T4. Bubble Tea Facebook"
+            title="T4. Facebook"
           >
             <img
               className="icon-adjustments float-end"
               src={FacebookIcon}
-              alt=" T4. Bubble Tea Facebook"
+              alt=" T4. Facebook"
             ></img>
           </a>
           <a
@@ -29,23 +29,23 @@ export default function PageHero(props) {
             href="https://www.instagram.com/t4_unitedkingdom/"
             target="_blank"
             rel="noreferrer"
-            title="T4. Bubble Tea Instagram"
+            title="T4. Instagram"
           >
             <img
-              className="icon-adjustments float-center"
+              className="icon-adjustments float-end"
               src={InstagramIcon}
-              alt="T4. Bubble Tea Instagram"
+              alt="T4. Instagram"
             ></img>
           </a>
         </div>
-        <section className="brand-name">
+        {/* <section className="brand-name">
           <h1 className="largeHeading white-text text-center ">
             T4. Bubble Tea
           </h1>
           <h2 className="subHeading secondary-text-color text-center">
             Crafted for professional brewing
           </h2>
-        </section>
+        </section> */}
         <MenuButton menuHref={props.menuHref} />
         <OrderGrid
           textColor="white"
@@ -53,10 +53,10 @@ export default function PageHero(props) {
           logo1={DeliverooLogo}
           logo2={PhoneIcon}
           link1={`https://deliveroo.co.uk/brands/t4/?utm_medium=backlink&utm_source=t4&utm_campaign=webreferral`}
-          content1={`Click here`}
+          content1={``}
           content2={`02085554090`}
           linkTitle1={`Order on Deliveroo`}
-          linkTitle2={`T4. Bubble Tea for Ordering`}
+          linkTitle2={`Call us to order`}
         />
       </div>
     </div>
